7468964192a7006fcf23af33e22dc77a2e94b6ff
samu235 <samuel2351994@hotmail.es>
2022-05-12 16:54:26 +0200

    add test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,41 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('view', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText("By Samuel");
   expect(linkElement).toBeInTheDocument();
+});
+
+test("suma", () => {
+  render(<App />);
+  
+  let buton9 = screen.getByText('9');
+  let butonadd = screen.getByText('+');
+  let butonEnt = screen.getByText('Enter');
+
+  fireEvent.click(buton9);
+  fireEvent.click(butonadd);
+  fireEvent.click(buton9);
+  fireEvent.click(butonEnt);
+
+  
+  expect(screen.getByText('18')).toBeInTheDocument();
+});
+
+test("resta", () => {
+  render(<App />);
+
+  let buton3 = screen.getByText('3');
+  let butonrest = screen.getByText('-');
+  let butonEnt = screen.getByText('Enter');
+
+  fireEvent.click(buton3);
+  fireEvent.click(buton3);
+  fireEvent.click(butonrest);
+  fireEvent.click(buton3);
+  fireEvent.click(butonEnt);
+
+  
+  expect(screen.getByText('30')).toBeInTheDocument();
 });
